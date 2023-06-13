bb0e5dcc14efef49757c1e11042ff28de15d1452
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-06-13 09:40:38 +0200

    complet searchBar Ui components

--- src/Components/SearchBar/Card.jsx
+++ src/Components/SearchBar/Card.jsx
@@ -3,9 +3,12 @@ import CardIcon from "../../assets/card.svg";
 
 const Card = () => {
   return (
-    <div className="flex items-center w-1/6 h-12 ">
-      <a href="#" className=" h-[120%] pl-30">
+    <div className=" flex items-center w-1/6 h-12 relative ">
+      <a href="#" className=" block h-[70%] pl-30">
         <img className="h-[90%] " src={CardIcon} alt="" />
+        <span className="bg-[#EE384E] rounded-full absolute  -top-2 left-2 px-1 text-white">
+          10
+        </span>
       </a>
     </div>
   );
