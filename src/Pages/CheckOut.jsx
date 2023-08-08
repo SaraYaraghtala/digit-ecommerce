499bc2c150068e6fdecdc577f2bcaebf791c147f
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-08-08 19:34:28 +0200

    added new page to my project about card and shoping basket

---
+++ src/Pages/CheckOut.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Basket from "../Components/Basket"
+
+const CheckOut = () => {
+    return (
+        <div>
+           < Basket />
+        </div>
+    );
+};
+
+export default CheckOut;
