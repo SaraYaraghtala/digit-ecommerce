c6af94e6a8d9db2c8a06b63e9a3e213b1ed9a3ba
Sara Yaraghtala <sarahyaraghtala@gmail.com>
2023-08-16 23:54:11 +0200

    create 2 button in this box

--- src/Components/Products/Box.jsx
+++ src/Components/Products/Box.jsx
@@ -10,13 +10,12 @@ const Box = (props) => {
   const basketItems = useSelector((state) => state.basket.items);
   useEffect(() => {
     const index = basketItems.findIndex((item) => {
-      return item.id == props.id;
+      return item.id === props.id;
     });
-    if (index>=0){
-      setQuantity(basketItems[index].count)
-    }else setQuantity(0)
+    if (index >= 0) {
+      setQuantity(basketItems[index].count);
+    } else setQuantity(0);
   }, [basketItems]);
-
 
   return (
     <div className="w-48 h-86 bg-white rounded-lg p-4 m-4 relative">
@@ -31,26 +30,32 @@ const Box = (props) => {
       <span className="bg-red-600 text-white top-1 left-0 -rotate-45 rounded-md absolute">
         {props.discount}%
       </span>
-   {  quantity>0? <div className="flex mt-2">
+      {quantity > 0 ? (
+        <div className="flex items-center justify-center mt-2  p-2  ">
+          <button
+            className="btn btn-sm btn-error ml-0"
+            onClick={() => dispatch(addToBasket(props))}
+          >
+            +
+          </button>
+          <span className="text-sm bg-red-600 text-black mx-2 px-2 py-1 rounded flex items-center ">
+            {quantity}
+          </span>
+          <button
+            className="btn btn-sm btn-error ml-0"
+            onClick={() => dispatch(removeFromBasket(props))}
+          >
+            _
+          </button>
+        </div>
+      ) : (
         <button
-          className="btn btn-sm btn-secondary ml-2"
+          className="btn btn-circle w-full mt-2"
           onClick={() => dispatch(addToBasket(props))}
         >
-          +
+          Add to Cart
         </button>
-        <span className="text-sm">{quantity}</span>
-        <button
-          className="btn btn-sm btn-secondary ml-2"
-          onClick={() => dispatch(removeFromBasket(props))}
-        >
-          _
-        </button>
-      </div>: <button
-        className="btn btn-success w-full mt-2"
-        onClick={() => dispatch(addToBasket(props))}
-      >
-        Add to Cart
-      </button>}
+      )}
     </div>
   );
 };
